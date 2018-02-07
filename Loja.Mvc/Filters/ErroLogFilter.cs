e31cb27bd157acdbddeaa974ee737bb927930e53
no1612 <no1612@4P16_12>
2018-02-07 18:49:21 -0200

    Aula 1

---
+++ Loja.Mvc/Filters/ErroLogFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Filters
+{
+    public class ErroLogFilter: HandleErrorAttribute
+    {
+
+
+    }
+}
