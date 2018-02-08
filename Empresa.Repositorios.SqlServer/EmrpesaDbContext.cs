7ee930af077a55d67f7cc13620c4238fb215372d
no1612 <no1612@4P16_12>
2018-02-07 22:45:46 -0200

    Aula 2

---
+++ Empresa.Repositorios.SqlServer/EmrpesaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Empresa.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Empresa.Repositorios.SqlServer
+{
+    public class EmrpesaDbContext : DbContext
+    {
+        public EmrpesaDbContext(DbContextOptions options): base(options)
+        {
+
+            Database.EnsureCreated();
+
+            
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
